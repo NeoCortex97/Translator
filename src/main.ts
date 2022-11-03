59bb1c0125170338d86ff0be5df6ee1098e20ce2
Stefan Schmelz <stefan_schmelz@gmx.net>
2022-11-03 03:42:40 +0100

    Made some Mods
    Gitignore hinzugefügt:
    	neue Datei:     .gitignore
    
    .parcel-cache aus dem tracking entfernt
    	gelöscht:       .parcel-cache/1baf53463255aa32
    	gelöscht:       .parcel-cache/31eaa7bbcdde5441
    	gelöscht:       .parcel-cache/3e143592a5abe84d
    	gelöscht:       .parcel-cache/5d102dff0a3f4e7e
    	gelöscht:       .parcel-cache/6548f2592c0b092a
    	gelöscht:       .parcel-cache/6bfd07989cfe07c9
    	gelöscht:       .parcel-cache/8ec8b337af6fb088
    	gelöscht:       .parcel-cache/994d8e47a1be7201
    	gelöscht:       .parcel-cache/9ff098162c63992b.txt
    	gelöscht:       .parcel-cache/data.mdb
    	gelöscht:       .parcel-cache/e58454aa5c0c5f06
    	gelöscht:       .parcel-cache/f6908a0790918965
    	gelöscht:       .parcel-cache/lock.mdb
    	gelöscht:       .parcel-cache/primary.crt
    	gelöscht:       .parcel-cache/private.pem
    
    dist aus dem tracking entfernt
    	gelöscht:       dist/Translator.59712999.js
    	gelöscht:       dist/Translator.59712999.js.map
    	gelöscht:       dist/index.4d6bcbeb.js
    	gelöscht:       dist/index.4d6bcbeb.js.map
    	gelöscht:       dist/index.61d3354a.css
    	gelöscht:       dist/index.61d3354a.css.map
    	gelöscht:       dist/index.b7a05eb9.js
    	gelöscht:       dist/index.b7a05eb9.js.map
    	gelöscht:       dist/index.html
    
    Save und load hinzugefügt
    	geändert:       src/index.html
    	geändert:       src/main.ts
    
    Start und install scripte hinzugefügt
    	neue Datei:     start.bat
    	neue Datei:	install.bat

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,73 @@ import ClipboardJS from 'clipboard';
 import './simple-grid.min.css';
 
 const preview = document.getElementById('content');
-const copyButton = document.getElementById('save');
+const copyButton = document.getElementById('copy');
+const saveButton = document.getElementById('save');
+const loadButton = document.getElementById('load');
+const loadDialog = document.getElementById('loadDialog');
+const loadSelect = document.getElementById('loadSelect');
+const loadOk = document.getElementById('ok');
 const parser = edjsParser({paragraph: text, raw: raw});
 const clipboard = new ClipboardJS('#copy');
 
 let refresh: any = undefined;
+
+window.addEventListener('load', (evt) => {
+    saveButton?.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        const name = prompt("Unter welcher Bezeichnung soll dieses Dokument gespeichert werden?", "temp");
+        editor.save().then((data) => {
+            console.log(`Saved to ${name}`);
+            if (name !== null)
+                window.localStorage.setItem(name, JSON.stringify(data.blocks));
+        }).catch((reason) => console.error(reason));
+    });
+
+    loadButton?.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        if (editor.blocks.getBlocksCount() !== 1 || !editor.blocks.getBlockByIndex(0).isEmpty) {
+            const toSave = confirm("Möchten die das aktuelle Dokument speichern?");
+            if (toSave) {
+                const saveName = prompt("Unter welcher Bezeichnung soll dieses Dokument gespeichert werden?", "temp");
+                editor.save().then((data) => {
+                    console.log(`Saved to ${saveName}`);
+                    if (saveName !== null)
+                        window.localStorage.setItem(saveName, JSON.stringify(data.blocks));
+                }).catch((reason) => console.error(reason));
+            }
+        }
+        let saves: Array<any> = [];
+        for (let i = 0; i < window.localStorage.length; i++) {
+            const element = window.localStorage.key(i);
+            saves.push(element)
+        }
+        console.log(saves);
+        if (loadSelect !== null)
+            loadSelect.innerHTML = "";
+        for (const item of saves) {
+            const option = document.createElement('option');
+            option.textContent = item
+            option.value = item;
+            loadSelect?.appendChild(option); 
+        }
+        
+        loadDialog?.showModal();
+    });
+
+    loadSelect?.addEventListener('change', (ev) => {
+        loadOk.value = loadSelect.value;
+    });
+
+    loadDialog?.addEventListener('close', (ev) => {
+        const doc = JSON.parse(window.localStorage.getItem(loadDialog.returnValue));
+        editor.blocks.clear()
+        console.log(doc)
+        for (const i of doc) {
+            editor.blocks.insert(i['type'], i['data'])
+        }
+        editor.blocks.delete(0);
+    });
+});
 
 const editor = new EditorJS({
     holder: 'markup',
